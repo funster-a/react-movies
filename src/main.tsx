dd9f8e3825f8ec64edfaf4da95197e10085ffc73
Amir Akhanov <axanov_a@inbox.ru>
2024-11-10 17:56:41 +0500

    dix folder structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 
+const router = createBrowserRouter([])
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <RouterProvider router={router} />
     <App />
   </StrictMode>,
 )
