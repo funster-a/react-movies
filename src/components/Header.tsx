0a6e7fa27154845622b4fc344779d5c16909aacd
Amir Akhanov <axanov_a@inbox.ru>
2024-11-11 22:00:51 +0500

    ads

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,34 @@
 import { Link } from "react-router-dom"
+import MyIcon from './MyIcon'
+import MyBell from "./MyBell"
+import s from './Header.module.css'
+import MyLogo from "./MyLogo"
 
 const Header = () => {
   return (
-    <div>
-        <div>
-            <Link to='/'>
-                
-                StreamVibe
-            </Link>
-            
-        </div>
+    <div className={s.navbar}>
+      <Link 
+        to='/'
+        className={s.logo}
+      >
+        <MyLogo/>
+      </Link>
 
-        <Link to='/'>Homepage</Link>
-        <Link to='/login'>Loginpage</Link>
+      <div className={s.menu}>
+        <Link 
+          to='/'
+          className={s.active}
+        >Homepage</Link>
+        <Link to='/login'></Link>
         <Link to='/moviesshows'>Movies&Shows</Link>
-        <Link to='/subscription'>Subscription</Link>
         <Link to='/support'>Support</Link>
+        <Link to='/subscription'>Subscription</Link>
+      </div>
 
-        <div className="icons">
-            
-        </div>
+      <div className={s.icons}>
+        <MyIcon/>
+        <MyBell/>
+      </div>
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './reset.css'
 import './index.css'
 import Homepage from './pages/Homepage.tsx'
 import Loginpage from './pages/Loginpage.tsx'
